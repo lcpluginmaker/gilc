0737647a2ab9ab71a438aff080c31dad9801bde5
alexcoder04 <alexcoder04@protonmail.com>
2022-06-09 19:14:13 +0200

    build framework

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 
 # gilc
 
-ILeoConsole re-implementation and plugin framework in Go
+**G**o **IL**eo**C**onsole - ILeoConsole re-implementation and plugin framework in Go
 
 Let's make Go a first-class language for LeoConsole plugins!
 
--- gilc.go
+++ gilc.go
@@ -3,6 +3,7 @@ package gilc
 import (
 	"encoding/base64"
 	"encoding/json"
+	"fmt"
 	"os"
 )
 
@@ -14,12 +15,53 @@ type IData struct {
 	Version            string `json:"Version"`
 }
 
-func ParseData() (IData, error) {
-	jsonBytes, err := base64.StdEncoding.DecodeString(os.Args[1])
+type IPlugin struct {
+	Name        string
+	Description string
+	Data        IData
+	Main        func(IData)
+	Command     func(IData, []string)
+	Shutdown    func(IData)
+}
+
+var Plugin IPlugin = IPlugin{}
+
+func ParseData() IData {
+	if len(os.Args) <= 2 {
+		fmt.Println("something went wrong, no arguments passed")
+		os.Exit(1)
+	}
+	jsonBytes, err := base64.StdEncoding.DecodeString(os.Args[2])
 	if err != nil {
-		return IData{}, err
+		os.Exit(1)
 	}
 	res := IData{}
 	err = json.Unmarshal(jsonBytes, &res)
-	return res, err
+	if err != nil {
+		os.Exit(1)
+	}
+	return res
+}
+
+func Setup(name string, description string, pmain func(IData), pcommand func(IData, []string), pshutdown func(IData)) {
+	Plugin.Name = name
+	Plugin.Description = description
+	Plugin.Data = ParseData()
+	Plugin.Main = pmain
+	Plugin.Command = pcommand
+	Plugin.Shutdown = pshutdown
+}
+
+func Run() {
+	switch os.Args[1] {
+	case "main":
+		Plugin.Main(Plugin.Data)
+		break
+	case "shutdown":
+		Plugin.Shutdown(Plugin.Data)
+		break
+	default:
+		Plugin.Command(Plugin.Data, os.Args[2:])
+		break
+	}
 }
